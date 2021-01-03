cfcc80d76fcdbbf52eeac1df864be3ff406ae1a3
scriptsandpngs <scriptsandpngs@gmail.com>
2021-01-03 18:47:15 +0100

    3/01

--- 1.js
+++ 1.js
@@ -3,19 +3,20 @@ let j2 = 0;
 
 function setup() {
   createCanvas(400, 400);
-  //frameRate(3)
-  background(250);
+  background(20);
 }
 
 function draw() {
   i2 += 1
-  j2 += 4
-  r = map(i2, 1, 200, 0, 50)
+  j2 += .1
+  r = map(i2, 1, 200, 200, 250)
   g = map(i2, 1, 50, 50, 100)
-  b = map(i2, 1, 200, 100, 250)
+  b = map(i2, 1, 200, 50, 100)
   stroke(r, random(g), b)
   let i = noise(width)
   let j = noise(width)
   strokeWeight(5)
-  point (noise(i) + i2, height/2 + cos(j2)* frameCount)
+  point (i2, height/2 + sin(j2) * 10)
+  point (i2, height/6 + sin(j2) * 10)
+  point (i2, height/1.2 + sin(j2) * 10)
 }
