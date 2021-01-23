7a5889b8966b7a83bb96e4652d68b81902754546
scriptsandpngs <scriptsandpngs@gmail.com>
2021-01-23 09:14:02 +0100

    rotate shits and stuff

--- 1.js
+++ 1.js
@@ -2,21 +2,17 @@ let i2 = 0;
 let j2 = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  background(20);
+  createCanvas(400, 400)
+  background(20)
+  rectMode(CENTER)
 }
 
 function draw() {
-  i2 += 1
-  j2 += .1
-  r = map(i2, 1, 200, 200, 250)
-  g = map(i2, 1, 50, 50, 100)
-  b = map(i2, 1, 200, 50, 100)
-  stroke(r, random(g), b)
-  let i = noise(width)
-  let j = noise(width)
-  strokeWeight(5)
-  point (i2, height/2 + sin(j2) * 10)
-  point (i2, height/6 + sin(j2) * 10)
-  point (i2, height/1.2 + sin(j2) * 10)
+    translate(width/2,height/2)
+    stroke(255)
+    push()
+    for (let i = 0; i < 6; i ++) {
+        line(0, 0, 30, 0)
+    }
+    pop()
 }
